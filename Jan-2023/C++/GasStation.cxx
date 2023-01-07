c253708afeb50a5413cf749176f98b17b082feab
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-01-07 15:44:45 +0530

    Create GasStation.cxx

---
+++ Jan-2023/C++/GasStation.cxx
@@ -0,0 +1,34 @@
+// 134. Gas Station
+class Solution
+{
+  public:
+	int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+	{
+		int totalGas = 0;
+		int totalCost = 0;
+		//should be able to travel atleast once in a clockwise direction according to question
+		for (int i = 0; i < cost.size(); i++)
+		{
+			totalGas += gas[i];
+			totalCost += cost[i];
+		}
+		if (totalGas < totalCost)
+		{
+			return -1;
+		}
+		//look for a positive value so you can travel to the next gas station, you should return start when you find it and make sure no other values make the remainingGas below 0 otherwise you cant move through all the stations
+		int remainingGas = 0;
+		int start = 0;
+		for (int i = 0; i < cost.size(); i++)
+		{
+			remainingGas += (gas[i] - cost[i]);
+			if (remainingGas < 0)
+			{
+				start = i + 1;
+				remainingGas = 0;
+			}
+		}
+		//this will start from a the gas station that can travel to all stations atleast once
+		return start;
+	}
+};

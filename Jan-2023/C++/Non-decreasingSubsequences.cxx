ce5da607ab4f95b71d915eafb70e05ee7e2d0212
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-01-20 18:31:13 +0530

    Update Non-decreasingSubsequences.cxx

--- Jan-2023/C++/Non-decreasingSubsequences.cxx
+++ Jan-2023/C++/Non-decreasingSubsequences.cxx
@@ -1,4 +1,27 @@
 
+/* Explanation
+
+In the bck method, check if the index has traversed through the entire nums array,
+
+if it has check if the length is greated than 2 or atleast 2 since we need to return
+
+subsequences and not a single element, if it exist add it into the final res array.
+
+Also use a set for non-repeating charactsrs. In the if statement below, check if the
+
+list is empty or check the previous element in the list with the current element in nums
+
+index. If it is smaller, add it into the list. Ex 3<5. 3 is the last element in the list
+
+and 5 is the element in nums[index], now perform backtracking and keep adding all elements,
+
+after bck is called remove the last element from the list to check all possible combinations,
+
+if it doesn't find any number smaller than current index called the bck method again outside
+
+loop. (If you still didn't understand, try out permutations before doing this problem)
+
+*/
 class Solution {
 	public:
 	vector<vector<int>> findSubsequences(vector<int> & nums) {

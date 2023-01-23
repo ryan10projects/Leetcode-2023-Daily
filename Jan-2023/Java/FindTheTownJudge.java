3b1fc7b0c72eec38dc7f02d42b08dd3d5c85224b
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-01-23 14:58:31 +0530

    Create FindTheTownJudge.java

---
+++ Jan-2023/Java/FindTheTownJudge.java
@@ -0,0 +1,30 @@
+// 
+
+997. Find the Town Judge
+// Used only one array, if -1, he/she trust someone, so not a candidate for town judge
+
+class Solution {
+
+    public int findJudge(int n, int[][] trust) {
+
+        int[] count = new int[n+1];
+
+        for(int[] i: trust){
+
+            count[i[0]]--;
+
+            count[i[1]]++;
+
+        }
+
+        for(int i=1;i<=n;i++){
+
+            if(count[i]==n-1) return i;
+
+         }
+
+         return -1;
+
+    }
+
+}

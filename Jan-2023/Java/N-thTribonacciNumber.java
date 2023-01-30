987e9cefc910c27ff901a66d55ad3a91eeb03e55
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-01-30 08:34:41 +0530

    Create N-thTribonacciNumber.java

---
+++ Jan-2023/Java/N-thTribonacciNumber.java
@@ -0,0 +1,24 @@
+// 1137. N-th Tribonacci Number
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n<=2) return 1;
+        int a=0;
+        int b=1;
+        int c=1;
+        int d=0;
+        while(n!=2){
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+            n--;
+        }
+        
+        return d;
+            
+        
+        
+            
+    }
+}

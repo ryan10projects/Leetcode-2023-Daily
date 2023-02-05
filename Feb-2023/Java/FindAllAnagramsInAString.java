57c565da431733b52b80d5e5aee10b535ff7c156
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-02-05 12:08:01 +0530

    Create FindAllAnagramsInAString.java

---
+++ Feb-2023/Java/FindAllAnagramsInAString.java
@@ -0,0 +1,33 @@
+// Find All Anagrams in a String
+class Solution {
+	public:
+	vector<int> findAnagrams(string s, string p) {
+		vector<int> res;
+		if (p.length() > s.length()) return res;
+		if (p == s) {
+			res.push_back(0);
+			return res;
+		}
+		int freq1[26];
+		int freq2[26];
+		int start = 0;
+		memset(freq1, 0, sizeof freq1);
+		memset(freq2, 0, sizeof freq2);
+		for (int i = 0; i < p.length(); i++) {
+			freq1[p[i] - 'a']++;
+			freq2[s[i] - 'a']++;
+		}
+		if (memcmp(freq1, freq2, sizeof(freq1)) == 0) {
+			res.push_back(start);
+		}
+		for (int i = p.length(); i < s.length(); i++) {
+			start++;
+			freq2[s[i] - 'a']++;
+			freq2[s[i - p.length()] - 'a']--;
+			if (memcmp(freq1, freq2, sizeof(freq1)) == 0) {
+				res.push_back(start);
+			}
+		}
+		return res;
+	}
+};

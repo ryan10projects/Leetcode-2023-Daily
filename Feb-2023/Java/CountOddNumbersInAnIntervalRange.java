569309a61717f4b274c0505458e622c44ee85ec0
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-02-13 08:33:48 +0530

    Create CountOddNumbersInAnIntervalRange.java

---
+++ Feb-2023/Java/CountOddNumbersInAnIntervalRange.java
@@ -0,0 +1,6 @@
+// 1523. Count Odd Numbers in an Interval Range
+// Assume half the numbers are odd
+// +1 because to round up for int
+public int countOdds(int low, int high) {
+        return (high + 1) / 2 - low / 2;
+    }

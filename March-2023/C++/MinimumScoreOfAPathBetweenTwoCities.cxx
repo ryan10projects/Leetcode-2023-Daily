21f2675de9783e834530798faaae756fbeb49bb1
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-04-01 00:24:11 +0530

    Update MinimumScoreOfAPathBetweenTwoCities.cxx

--- March-2023/C++/MinimumScoreOfAPathBetweenTwoCities.cxx
+++ March-2023/C++/MinimumScoreOfAPathBetweenTwoCities.cxx
@@ -1,4 +1,6 @@
 //Minimum Score of a Path Between Two Cities
+// we cannot find the minimum distance in the array directly because the graph is not necessarily connected, which means that some cities might not be reachable from others. So, if we loop through the array and find the minimum distance, we might miss some cities and therefore not be able to find the minimum score of a path between cities 1 and n.
+// Instead, we need to traverse the graph using a search algorithm, such as depth-first search (DFS) or breadth-first search (BFS), to find all the possible paths between cities 1 and n and calculate the minimum score of each path. This ensures that we visit all the cities and find the minimum score of a path between cities 1 and n in the entire graph.
 class Solution {
 public:
 

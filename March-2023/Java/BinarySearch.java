86024b4aadf61de7bfd73b55c3bd22d056d77d9c
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-04-01 15:34:12 +0530

    Create BinarySearch.java

---
+++ March-2023/Java/BinarySearch.java
@@ -0,0 +1,21 @@
+// Binary Search
+class Solution {
+    public int search(int[] nums, int target) {      
+        int i;
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;                
+            if(target==nums[mid]){
+                return mid;            
+                }
+            else if(target>nums[mid]){
+                low=mid+1;                
+                }
+            else{
+                high=mid-1;                
+                }                 
+              }
+   			  return -1;       
+        }   
+}

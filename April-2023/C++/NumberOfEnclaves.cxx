9a5ef19950424df8c5db30707bfad144187a00de
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-04-08 23:35:03 +0530

    Create NumberOfEnclaves.cxx

---
+++ April-2023/C++/NumberOfEnclaves.cxx
@@ -0,0 +1,45 @@
+// 1020. Number of Enclaves
+class Solution {
+    int count = 0;
+public:
+    void dfs(vector<vector<int>>& grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==0) return;
+        
+        grid[i][j]=0;
+        
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
+    
+    int numEnclaves(vector<vector<int>>& grid) {
+        for(int row=0; row<grid.size(); row++){
+            if(grid[row][0]==1){
+                dfs(grid,row,0);
+            }
+            if(grid[row][grid[0].size()-1]==1){
+                dfs(grid,row,grid[0].size()-1);
+            }
+        }
+        
+        for(int col=0; col<grid[0].size(); col++){
+            if(grid[0][col]==1){
+                dfs(grid,0,col);
+            }
+            if(grid[grid.size()-1][col]==1){
+                dfs(grid,grid.size()-1,col);
+            }
+        }
+        
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(grid[i][j]==1){
+                    count++;
+                }
+            }
+        }
+         
+        return count;
+    }
+};

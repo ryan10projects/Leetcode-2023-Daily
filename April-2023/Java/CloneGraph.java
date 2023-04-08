3dbaceea5a7c71ac9fe840e1527d8ef42ef6ec17
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-04-08 20:57:37 +0530

    Create CloneGraph.java

---
+++ April-2023/Java/CloneGraph.java
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+// 133. Clone Graph
+// Algorithmn
+// Use a HashMap to store the original node as key and value will be the clone
+// constant time insertion, deletion
+// 
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node==null) return null;
+        HashMap<Integer,Node> map = new HashMap<>();
+        return dfs(map,node);
+        }
+       public Node dfs(HashMap<Integer,Node> map, Node n){
+           if(map.containsKey(n.val)) return map.get(n.val);          
+              Node copy = new Node(n);
+              map.put(n.val,copy);
+           //map original node as key and vopy as value
+              for(Node nei:n.neighbors){
+      
+
+ copy.neighbors.add(dfs(map,nei));
+                  }
+              return copy;           
+           }      
+    }

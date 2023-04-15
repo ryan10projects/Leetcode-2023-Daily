b41302c29dc93ab93fef162f71f4d489137d8934
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-04-15 17:03:42 +0530

    Update MaximumValueOfKCoinsFromPiles.java

--- April-2023/Java/MaximumValueOfKCoinsFromPiles.java
+++ April-2023/Java/MaximumValueOfKCoinsFromPiles.java
@@ -17,6 +17,9 @@ class Solution {
 		int not_taken = solve(piles, i + 1, k);
 		int sum = 0;
 		int taken = 0;
+// For example, if k is 5 and the size of the current pile is 3, then we can only take up to 3 items from the pile. In this case, Math.min(list.size(), k) will return 3, so the for loop will iterate over the first 3 elements of the pile.
+
+// If k is 2 and the size of the current pile is 5, then we can take up to 2 items from the pile. In this case, Math.min(list.size(), k) will return 2, so the for loop will only iterate over the first 2 elements of the pile.
 		for (int j = 0; j < Math.min(k, piles.get(i).size()); j++) {
 			sum += piles.get(i).get(j);
 			// take the pile

b4b3bfe77059e84dd6893014446d3a5c22fb1419
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-04-26 09:02:46 +0530

    Create AddDigits.java

---
+++ April-2023/Java/AddDigits.java
@@ -0,0 +1,18 @@
+// 258. Add Digits
+
+// Keep calling the function until you have met your desired goal
+// Here, we need the answer to be in single digit, so we've added <=9 as the goal
+
+class Solution {
+    public int addDigits(int num) {
+        int ans = 0;
+        int glob = num;
+        if(glob<=9) return glob;
+         while(num!=0){
+		 // fetch last digit
+          ans += num%10;
+		  // fetch all the digits except the last digit
+          num = num/10; }
+        return addDigits(ans);
+    }
+}

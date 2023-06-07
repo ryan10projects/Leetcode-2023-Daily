970f95a6979f7d2edb4ff5d5ea9dcf9e7ef47a6e
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-06-07 16:28:35 +0530

    Create MinimumFlipsToMakeAOrBEqualToC.java

---
+++ June-2023/Java/MinimumFlipsToMakeAOrBEqualToC.java
@@ -0,0 +1,25 @@
+// 1318. Minimum Flips to Make a OR b Equal to c
+// Algorithm 
+// Perform Or operation
+// use x&1 to get the right most bit
+// use >> for deleting the right most bit
+// compare if Or operation gives the results of both in c, if not flip
+
+class Solution {
+    public int minFlips(int a, int b, int c) {
+        int flips = 0;
+        while(c!=0 || b!=0 || a!=0){
+            if((c&1)==1){
+                if((a&1)==0 && (b&1)==0) flips++;
+            }
+            else if((c&1)==0){
+                if((a&1)==1) flips++;
+                if((b&1)==1) flips++;
+            }
+            c>>=1;
+            b>>=1;
+            a>>=1;
+        }
+        return flips;
+    }
+}

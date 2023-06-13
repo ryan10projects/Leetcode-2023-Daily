4bf90726a7ac3a3a4f7979c50b8538ce7fdc2780
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-06-13 12:50:01 +0530

    Create EqualRowAndColumnPairs.java

---
+++ June-2023/Java/EqualRowAndColumnPairs.java
@@ -0,0 +1,35 @@
+// 2352. Equal Row and Column Pairs
+class Solution {
+	public int equalPairs(int[][] grid) {
+		HashMap<List<Integer>, Integer> map = new HashMap<>();
+
+        // looping through rows
+		for (int r = 0; r < grid.length; r++) {
+			List<Integer> ls = new LinkedList<>();
+			for (int c = 0; c < grid[0].length; c++) {
+				ls.add(grid[r][c]);
+			}
+			if (!map.containsKey(ls)) {
+				map.put(ls, 0);
+			}
+			map.put(ls, map.getOrDefault(ls, 0) + 1);
+		}
+		
+		//storing count
+		int count = 0;
+		
+        // looping through columns
+		for (int c = 0; c < grid.length; c++) {
+			List<Integer> ls = new LinkedList<>();
+			for (int r = 0; r < grid[0].length; r++) {
+				ls.add(grid[r][c]);
+			}
+			if (map.containsKey(ls)) {
+				count += map.get(ls);
+			}
+
+		}
+		return count;
+
+	}
+}

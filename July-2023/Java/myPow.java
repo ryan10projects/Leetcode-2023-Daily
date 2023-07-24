995aa8c6ca67b3b92cefc60ab296d47934db09b3
Ryan Rodricks <59692861+ryan10projects@users.noreply.github.com>
2023-07-24 23:59:14 +0530

    Create myPow.java

---
+++ July-2023/Java/myPow.java
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==INT_MIN){
+            n/=2;
+            x*=x;
+         }
+        if(n==0) return 1;
+        if(n<0){
+            x=1/x;
+            n=-n;
+        }
+        return n%2==0? myPow(x*x,n/2): x*myPow(x*x,n/2);
+    }
+};
